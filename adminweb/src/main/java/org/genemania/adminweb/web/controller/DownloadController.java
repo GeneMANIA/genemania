9090e4e0e607ab1aa1fc73eb771b859d7b3f7abe
Khalid Zuberi <kzuberi@gmail.com>
2014-05-27 15:32:36 -0400

    rename download method since generic, remove dead code

--- adminweb/src/main/java/org/genemania/adminweb/web/controller/DownloadController.java
+++ adminweb/src/main/java/org/genemania/adminweb/web/controller/DownloadController.java
@@ -38,65 +38,8 @@ public class DownloadController extends BaseController {
     @Autowired
     private DatamartDb dmdb;
 
-    /*
-    @RequestMapping(method = RequestMethod.GET, value = "/download/network")
-    public void getNetworkFile(
-        @RequestParam("oid") Long organismId,
-        @RequestParam("id") Long networkId,
-        @RequestParam(value="desc", defaultValue="false") boolean descriptions,
-        HttpServletResponse response) throws DatamartException, IOException, SQLException {
-
-        Network network = dmdb.getNetworkDao().queryForId((int) networkId.longValue());
-
-        if (descriptions) { // request is for corresponding metadata file
-            AttributeMetadata md = network.getAttributeMetadata();
-            if (md == null) {
-                throw new DatamartException("no file available");
-            }
-            else {
-                dmdb.getAttributeMetadataDao().refresh(md);
-            }
-
-            String filename = md.getOriginalFilename();
-
-            response.setContentType("text/plain");
-            response.setHeader("Content-Disposition", String.format("attachment; filename=%s", filename));
-
-            File file = fileStorageService.getFile(md.getFilename());
-            IOUtils.copy(new FileInputStream(file), response.getOutputStream());
-            response.flushBuffer();
-        }
-        else { // network
-            String filename = network.getOriginalFilename();
-
-            response.setContentType("text/plain");
-            response.setHeader("Content-Disposition", String.format("attachment; filename=%s", filename));
-
-            File file = fileStorageService.getFile(network.getFilename());
-            IOUtils.copy(new FileInputStream(file), response.getOutputStream());
-            response.flushBuffer();
-        }
-    }
-
-    @RequestMapping(method = RequestMethod.GET, value = "/download/identifiers")
-    public void getIdentifiersFile(
-        @RequestParam("oid") Long organismId,
-        @RequestParam("id") Long identifiersId,
-        HttpServletResponse response) throws DatamartException, IOException, SQLException {
-        response.setContentType("text/plain");
-
-        Identifiers identifiers = dmdb.getIdentifiersDao().queryForId((int)identifiersId.longValue());
-        String filename = identifiers.getOriginalFilename();
-        response.setHeader("Content-Disposition", String.format("attachment; filename=%s", filename));
-
-        File file = fileStorageService.getFile(identifiers.getFilename());
-        IOUtils.copy(new FileInputStream(file), response.getOutputStream());
-        response.flushBuffer();
-    }
-    */
-
     @RequestMapping(method = RequestMethod.GET, value = "/download/file")
-    public void getIdentifiersFile(@RequestParam("id") int fileId,
+    public void downloadFile(@RequestParam("id") int fileId,
         HttpServletResponse response) throws DatamartException, IOException, SQLException {
         response.setContentType("text/plain");
 
