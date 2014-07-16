284eb45772f853070d884db8c55a742f56ede4f3
Max <maxkfranz@gmail.com>
2014-07-16 16:01:54 -0400

    more permissive cors

--- website/src/main/java/org/genemania/controller/rest/CorsFilter.java
+++ website/src/main/java/org/genemania/controller/rest/CorsFilter.java
@@ -30,7 +30,7 @@ public class CorsFilter implements Filter {
 		response.setHeader("Access-Control-Allow-Origin", "*");
 		response.setHeader("Access-Control-Allow-Methods", "POST, GET");
 		response.setHeader("Access-Control-Max-Age", "0");
-		response.setHeader("Access-Control-Allow-Headers", "x-requested-with");
+		response.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 		
 		chain.doFilter(req, res);
 	}
