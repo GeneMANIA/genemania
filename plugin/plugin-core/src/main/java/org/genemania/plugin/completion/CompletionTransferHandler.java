e03509202ff768af3ddbb9f028d51285dd05e0d3
Christian Lopes <chrtannus@gmail.com>
2015-05-26 17:01:53 -0400

    More UI improvements

--- plugin/plugin-core/src/main/java/org/genemania/plugin/completion/CompletionTransferHandler.java
+++ plugin/plugin-core/src/main/java/org/genemania/plugin/completion/CompletionTransferHandler.java
@@ -19,27 +19,29 @@
 
 package org.genemania.plugin.completion;
 
-import java.awt.Component;
+import static javax.swing.GroupLayout.DEFAULT_SIZE;
+import static javax.swing.GroupLayout.PREFERRED_SIZE;
+
 import java.awt.Container;
 import java.awt.Dialog;
 import java.awt.Dimension;
 import java.awt.Frame;
-import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
-import java.awt.Insets;
 import java.awt.datatransfer.DataFlavor;
 import java.awt.datatransfer.Transferable;
 import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Set;
 import java.util.StringTokenizer;
-import java.util.Map.Entry;
 
-import javax.swing.BorderFactory;
+import javax.swing.AbstractAction;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.GroupLayout.ParallelGroup;
+import javax.swing.GroupLayout.SequentialGroup;
 import javax.swing.JButton;
 import javax.swing.JComponent;
 import javax.swing.JDialog;
@@ -59,15 +61,22 @@ import org.genemania.plugin.task.TaskDispatcher;
 import org.genemania.plugin.view.util.UiUtils;
 import org.genemania.util.ProgressReporter;
 
+@SuppressWarnings("serial")
 public class CompletionTransferHandler extends TransferHandler {
-	private static final long serialVersionUID = 1L;
+	
 	private final GeneCompletionProvider2 provider;
 	private final CompletionConsumer consumer;
 	private final NetworkUtils networkUtils;
 	private final UiUtils uiUtils;
 	private final TaskDispatcher taskDispatcher;
 
-	public CompletionTransferHandler(GeneCompletionProvider2 provider, CompletionConsumer consumer, NetworkUtils networkUtils, UiUtils uiUtils, TaskDispatcher taskDispatcher) {
+	public CompletionTransferHandler(
+			final GeneCompletionProvider2 provider,
+			final CompletionConsumer consumer,
+			final NetworkUtils networkUtils,
+			final UiUtils uiUtils,
+			final TaskDispatcher taskDispatcher
+	) {
 		this.provider = provider;
 		this.consumer = consumer;
 		this.networkUtils = networkUtils;
@@ -78,6 +87,7 @@ public class CompletionTransferHandler extends TransferHandler {
 	@Override
 	public boolean importData(final JComponent comp, final Transferable t) {
 		final PasteResult[] result = new PasteResult[1];
+		
 		GeneManiaTask task = new GeneManiaTask(Strings.completionTransferHandler_title2) {
 			@Override
 			protected void runTask() throws Throwable {
@@ -85,16 +95,20 @@ public class CompletionTransferHandler extends TransferHandler {
 				result[0] = validate(data, progress);
 			}
 		};
+		
 		Frame parent = uiUtils.getFrame(comp);
 		taskDispatcher.executeTask(task, parent, true, true);
+		
 		if (result[0] != null && result[0].hasIssues()) {
 			showIssues(result[0], parent);
 		}
+		
 		return super.importData(comp, t);
 	}
 
 	private JDialog createDialog(Container parent) {
 		JDialog dialog = null;
+		
 		while (parent != null) {
 			if (parent instanceof Frame) {
 				dialog = new JDialog((Frame) parent, true);
@@ -105,78 +119,114 @@ public class CompletionTransferHandler extends TransferHandler {
 			}
 			parent = parent.getParent();
 		}
-		if (dialog == null) {
+		
+		if (dialog == null)
 			dialog = new JDialog();
-		}
+		
 		return dialog;
 	}
 	
 	private void showIssues(PasteResult result, Container parent) {
 		final JDialog dialog = createDialog(parent);
 		dialog.setTitle(Strings.completionTransferHandler_title);
-
-		JPanel panel = uiUtils.createJPanel();
-		panel.setBorder(BorderFactory.createEmptyBorder(32, 32, 32, 32));
-		panel.setLayout(new GridBagLayout());
-
-		int xOffset = 0;
 		
-		Insets insets = new Insets(0, 4, 0, 4);
-		Map<String, Set<String>> synonyms = result.getSynonyms();
-		if (synonyms.size() > 0) {
-			JLabel label = new JLabel(Strings.completionTransferHandlerSynonyms_label);
-			panel.add(label, new GridBagConstraints(xOffset, 0, 1, 1, 1, 0, GridBagConstraints.CENTER, GridBagConstraints.HORIZONTAL, insets, 0, 0));
-			
-			List<String> symbols = new ArrayList<String>();
-			for (Entry<String, Set<String>> entry : synonyms.entrySet()) {
-				String preferredSymbol = entry.getKey();
-				for (String symbol : entry.getValue()) {
-					symbols.add(String.format("%s (%s)", symbol, preferredSymbol)); //$NON-NLS-1$
-				}
-			}
-			Collections.sort(symbols);
-			JScrollPane scrollPane = new JScrollPane(new JList(new DynamicListModel<String>(symbols)));
-			panel.add(scrollPane, new GridBagConstraints(xOffset, 1, 1, 1, 1, 1, GridBagConstraints.CENTER, GridBagConstraints.BOTH, insets, 0, 0));
-			xOffset++;
-		}
-		
-		List<String> unrecognizedGenes = new ArrayList<String>(result.getUnrecognizedSymbols());
-		if (unrecognizedGenes.size() > 0) {
-			JLabel label = new JLabel(Strings.completionTransferHandlerUnrecognizedGenes_label);
-			panel.add(label, new GridBagConstraints(xOffset, 0, 1, 1, 1, 0, GridBagConstraints.CENTER, GridBagConstraints.HORIZONTAL, insets, 0, 0));
-			
-			Collections.sort(unrecognizedGenes);
-			JScrollPane scrollPane = new JScrollPane(new JList(new DynamicListModel<String>(unrecognizedGenes)));
-			panel.add(scrollPane, new GridBagConstraints(xOffset, 1, 1, 1, 1, 1, GridBagConstraints.CENTER, GridBagConstraints.BOTH, insets, 0, 0));
-			xOffset++;
-		}
-		
-		Map<String, Integer> duplicates = result.getDuplicates();
-		if (duplicates.size() > 0) {
-			JLabel label = new JLabel(Strings.completionTransferHandlerDuplicateGenes_label);
-			panel.add(label, new GridBagConstraints(xOffset, 0, 1, 1, 1, 0, GridBagConstraints.CENTER, GridBagConstraints.HORIZONTAL, insets, 0, 0));
-			
-			List<String> duplicateGenes = new ArrayList<String>();
-			for (Entry<String, Integer> entry : duplicates.entrySet()) {
-				duplicateGenes.add(String.format("%s (%d)", entry.getKey(), entry.getValue())); //$NON-NLS-1$
-			}
-			Collections.sort(duplicateGenes);
-			JScrollPane scrollPane = new JScrollPane(new JList(new DynamicListModel<String>(duplicateGenes)));
-			panel.add(scrollPane, new GridBagConstraints(xOffset, 1, 1, 1, 1, 1, GridBagConstraints.CENTER, GridBagConstraints.BOTH, insets, 0, 0));
-			xOffset++;
-		}
-
-		JButton button = new JButton(Strings.completionTransferHandlerOkButton_label);
-		button.setAlignmentX(Component.CENTER_ALIGNMENT);
-		button.addActionListener(new ActionListener() {
+		final JButton button = new JButton(new AbstractAction(Strings.completionTransferHandlerOkButton_label) {
+			@Override
 			public void actionPerformed(ActionEvent e) {
 				dialog.setVisible(false);
 			}
 		});
-		panel.add(button, new GridBagConstraints(0, 2, xOffset, 1, 0, 0, GridBagConstraints.CENTER, GridBagConstraints.NONE, new Insets(0, 0, 0, 0), 0, 0));
 		
-		Container contents = dialog.getContentPane();
-		contents.add(panel);
+		final JPanel buttonPanel = uiUtils.createOkCancelPanel(null, button);
+		
+		final JPanel panel = uiUtils.createJPanel();
+		final GroupLayout layout = new GroupLayout(panel);
+        panel.setLayout(layout);
+		layout.setAutoCreateGaps(true);
+		layout.setAutoCreateContainerGaps(true);
+		
+		final SequentialGroup hgroup = layout.createSequentialGroup();
+		final ParallelGroup vgroup = layout.createParallelGroup(Alignment.LEADING, true);
+		
+		layout.setHorizontalGroup(layout.createParallelGroup(Alignment.CENTER, true)
+				.addGroup(hgroup)
+				.addComponent(buttonPanel, DEFAULT_SIZE, DEFAULT_SIZE, Short.MAX_VALUE)
+		);
+		layout.setVerticalGroup(layout.createSequentialGroup()
+				.addGroup(vgroup)
+				.addComponent(buttonPanel, PREFERRED_SIZE, DEFAULT_SIZE, PREFERRED_SIZE)
+		);
+
+		final Map<String, Set<String>> synonyms = result.getSynonyms();
+		
+		if (synonyms.size() > 0) {
+			final JLabel label = new JLabel(Strings.completionTransferHandlerSynonyms_label);
+			final List<String> symbols = new ArrayList<String>();
+			
+			for (Entry<String, Set<String>> entry : synonyms.entrySet()) {
+				String preferredSymbol = entry.getKey();
+				
+				for (String symbol : entry.getValue())
+					symbols.add(String.format("%s (%s)", symbol, preferredSymbol)); //$NON-NLS-1$
+			}
+			
+			Collections.sort(symbols);
+			final JScrollPane scrollPane = new JScrollPane(new JList(new DynamicListModel<String>(symbols)));
+			
+			hgroup.addGroup(layout.createParallelGroup(Alignment.LEADING, true)
+					.addComponent(label, PREFERRED_SIZE, DEFAULT_SIZE, PREFERRED_SIZE)
+					.addComponent(scrollPane, DEFAULT_SIZE, DEFAULT_SIZE, Short.MAX_VALUE)
+			);
+			vgroup.addGroup(layout.createSequentialGroup()
+					.addComponent(label, PREFERRED_SIZE, DEFAULT_SIZE, PREFERRED_SIZE)
+					.addComponent(scrollPane, DEFAULT_SIZE, DEFAULT_SIZE, Short.MAX_VALUE)
+			);
+		}
+		
+		final List<String> unrecognizedGenes = new ArrayList<String>(result.getUnrecognizedSymbols());
+		
+		if (unrecognizedGenes.size() > 0) {
+			final JLabel label = new JLabel(Strings.completionTransferHandlerUnrecognizedGenes_label);
+			
+			Collections.sort(unrecognizedGenes);
+			final JScrollPane scrollPane = new JScrollPane(new JList(new DynamicListModel<String>(unrecognizedGenes)));
+			
+			hgroup.addGroup(layout.createParallelGroup(Alignment.LEADING, true)
+					.addComponent(label, PREFERRED_SIZE, DEFAULT_SIZE, PREFERRED_SIZE)
+					.addComponent(scrollPane, DEFAULT_SIZE, DEFAULT_SIZE, Short.MAX_VALUE)
+			);
+			vgroup.addGroup(layout.createSequentialGroup()
+					.addComponent(label, PREFERRED_SIZE, DEFAULT_SIZE, PREFERRED_SIZE)
+					.addComponent(scrollPane, DEFAULT_SIZE, DEFAULT_SIZE, Short.MAX_VALUE)
+			);
+		}
+		
+		Map<String, Integer> duplicates = result.getDuplicates();
+		if (duplicates.size() > 0) {
+			final JLabel label = new JLabel(Strings.completionTransferHandlerDuplicateGenes_label);
+			final List<String> duplicateGenes = new ArrayList<String>();
+			
+			for (Entry<String, Integer> entry : duplicates.entrySet()) {
+				duplicateGenes.add(String.format("%s (%d)", entry.getKey(), entry.getValue())); //$NON-NLS-1$
+			}
+			
+			Collections.sort(duplicateGenes);
+			final JScrollPane scrollPane = new JScrollPane(new JList(new DynamicListModel<String>(duplicateGenes)));
+			
+			hgroup.addGroup(layout.createParallelGroup(Alignment.LEADING, true)
+					.addComponent(label, PREFERRED_SIZE, DEFAULT_SIZE, PREFERRED_SIZE)
+					.addComponent(scrollPane, DEFAULT_SIZE, DEFAULT_SIZE, Short.MAX_VALUE)
+			);
+			vgroup.addGroup(layout.createSequentialGroup()
+					.addComponent(label, PREFERRED_SIZE, DEFAULT_SIZE, PREFERRED_SIZE)
+					.addComponent(scrollPane, DEFAULT_SIZE, DEFAULT_SIZE, Short.MAX_VALUE)
+			);
+		}
+
+		dialog.getContentPane().add(panel);
+		
+		uiUtils.setDefaultOkCancelKeyStrokes(dialog.getRootPane(), button.getAction(), button.getAction());
+		dialog.getRootPane().setDefaultButton(button);
 		
 		dialog.setMinimumSize(new Dimension(300, 300));
 		dialog.pack();
@@ -191,12 +241,13 @@ public class CompletionTransferHandler extends TransferHandler {
 		
 		try {
 			while (tokenizer.hasMoreTokens()) {
-				if (progress.isCanceled()) {
+				if (progress.isCanceled())
 					return result;
-				}
+				
 				String token = tokenizer.nextToken();
 				result.addDuplicate(token);
 				Gene gene = provider.getGene(token);
+				
 				if (gene != null) {
 					result.addSynonym(gene);
 					consumer.consume(gene.getSymbol());
@@ -204,11 +255,13 @@ public class CompletionTransferHandler extends TransferHandler {
 				} else {
 					result.addUnrecognizedSymbol(token);
 				}
+				
 				progress.setStatus(String.format(Strings.completionTransferHandler_status, valid, result.getUnrecognizedSymbols().size()));
 			}
 		} finally {
 			consumer.finish();
 		}
+		
 		return result;
 	}
 }
