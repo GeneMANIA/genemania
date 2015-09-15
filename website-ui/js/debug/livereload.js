037cf50784237f7dc897a313a87a1347239e2b9f
Max Franz <maxkfranz@gmail.com>
2015-09-15 13:37:30 -0400

    improved livereload & safer mvn-gulp script

--- website-ui/js/debug/livereload.js
+++ website-ui/js/debug/livereload.js
@@ -2,5 +2,5 @@ document.addEventListener('DOMContentLoaded', function(){
   var script = document.createElement('script');
   script.src = 'http://' + window.location.hostname + ':35729/livereload.js';
 
-  document.head.appendChild( script );
+  document.head.insertBefore( script, document.head.firstChild );
 });
--- website/scripts/copy-webapp-via-gulp.sh
+++ website/scripts/copy-webapp-via-gulp.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-npm install
-gulp website
+NPM=`which npm` || /usr/local/bin/npm
+GULP=`which gulp` || /usr/local/bin/gulp
+
+$NPM install
+$GULP website
